af36b43c384223bdda72e06db779ffb282439198
Shlok <saraogishlok5@gmail.com>
2023-03-09 10:24:46 +0530

    header

--- client/src/components/Headers/Headers.js
+++ client/src/components/Headers/Headers.js
@@ -8,7 +8,7 @@ const Headers = () => {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">Intern Tracker</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/register">Register</Nav.Link>
